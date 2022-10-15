fe96cb50ed2843c427aac894adb3f05680844de3
Ruslan <ruslan_feoktistov@mail.ru>
2022-10-15 14:28:17 +0400

    gui

---
+++ gui.py
@@ -0,0 +1,8 @@
+import pycache.div_mod as div
+import pycache.mult_mod as mul
+import pycache.minus_mod as minus
+import pycache.sum_mod as sum
+
+
+dict_ar = {'/': div, '*': mul, '-': minus, '+': sum}
+dict_log = {'/': 'деление', '*': 'умножение', '-': 'вычитание', '+': 'сложение'}
